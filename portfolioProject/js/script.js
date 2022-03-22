8fe2d3cc4be928bdb0e15f99934562cd9e263d64
Julian Mercado <julianmercadomeza@gmail.com>
2022-03-22 17:15:27 -0400

    create home page

--- portfolioProject/js/script.js
+++ portfolioProject/js/script.js
@@ -6,7 +6,7 @@ const allSections = document.querySelector(".main-content");
 function PageTransitions() {
   // Add active class to clicked button
   for (let i = 0; i < sectBtn.length; i++) {
-    sectBtn[i].addEventListener("click", () => {
+    sectBtn[i].addEventListener("click", function () {
       let currentBtn = document.querySelectorAll(".active-btn");
       currentBtn[0].className = currentBtn[0].className.replace(
         "active-btn",
@@ -15,6 +15,27 @@ function PageTransitions() {
       this.className += " active-btn";
     });
   }
+
+  //Active Sections
+  allSections.addEventListener("click", (e) => {
+    console.log(e.target);
+    const id = e.target.dataset.id;
+    console.log(e.target.dataset.id);
+    if (id) {
+      sectBtns.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+      e.target.classList.add("active");
+
+      //hide other sections
+      sections.forEach((section) => {
+        section.classList.remove("active");
+      });
+
+      const element = document.getElementById(id);
+      element.classList.add("active");
+    }
+  });
 }
 
 PageTransitions();
